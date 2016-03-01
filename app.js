a6431a89ea1609325a1a7a198db3bb46202d0551
unknown <gagandeeps404>
2016-02-29 21:17:46 -0800

    update

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function()
+{
+  $('#searchTerm').keyup(function()
+  {
+    searchTable($(this).val());
+  });
+});
+ 
+function searchTable(inputVal)
+{
+  var table = $('#prodInfo');
+  table.find('tr').each(function(index, row)
+  {
+    var allCells = $(row).find('td');
+    if(allCells.length)
+    {
+      var found = false;
+      allCells.each(function(index, td)
+      {
+        var regExp = new RegExp(inputVal, 'i');
+        if(regExp.test($(td).text()))
+        {
+          found = true;
+          return false;
+        }
+      });
+      if(found == true)$(row).show();else $(row).hide();
+    }
+  });
+}
